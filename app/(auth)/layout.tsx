e0f6243724c3bce0fa00e9bd77fb20eacffa9645
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-01 15:51:13 +0545

    refactor

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface AuthLayoutProps {
+  children: React.ReactNode
+}
+
+const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
+  return <div className='flex items-center justify-center'>{children}</div>
+}
+
+export default AuthLayout
